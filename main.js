2a7e2b62f665af8900dcade6233cdf028e7a63bc
Sorrel Brigman <sorrel.k@gmail.com>
2017-04-15 12:34:12 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+  xlsxj = require("xlsx-to-json");
+  xlsxj({
+    input: "xl_files/duluth.xlsx",
+    output: "json/output.json"
+  }, function(err, result) {
+    if(err) {
+      console.error(err);
+    }else {
+      console.log(result);
+    }
+  });
